ea9756f93061c3cbccabedc6e5ea6178f480fac2
kislyakov-andrei <kisli@mail.ru>
2024-10-12 16:09:39 +0300

    Lesson 2

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+   // Task 1
+    int a=654321;
+    byte b=127;
+    short sh= -32000;
+    long ln=987654321;
+    float f=3.14E+12f;
+    double dbl= 3.418E01;
+    System.out.println("Task 1");
+    System.out.println("Значение переменной 'a' с типом 'int' равно " + a);
+    System.out.println("Значение переменной 'b' с типом 'byte' равно " + b);
+    System.out.println("Значение переменной 'sh' с типом 'short' равно " + sh);
+    }
+ }
