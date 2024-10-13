eabf4b0771835d84b6e958d83c25f933a8c9a64d
kislyakov-andrei <kisli@mail.ru>
2024-10-13 22:42:35 +0300

    Lesson 2

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,40 @@ public class Main {
     byte b=127;
     short sh= -32000;
     long ln=987654321;
-    float f=3.14E+12f;
+    float f=3.75f;
     double dbl= 3.418E01;
     System.out.println("Task 1");
     System.out.println("Значение переменной 'a' с типом 'int' равно " + a);
     System.out.println("Значение переменной 'b' с типом 'byte' равно " + b);
     System.out.println("Значение переменной 'sh' с типом 'short' равно " + sh);
+    System.out.println("Значение переменной 'ln' с типом 'long' равно " + ln);
+    System.out.println("Значение переменной 'f' с типом 'float' равно " + f);
+    System.out.println("Значение переменной 'dbl' с типом 'double' равно " + dbl);
+    //Task 2
+    System.out.println("Task 2");
+    long ln2=987678965549l;
+    double dbl2=27.12;
+    short sh2=569;
+    int i2=-159;
+    short sh3=27897;
+    byte b2=67;
+    float fl2= 2.768f;
+    System.out.println(fl2);
+    System.out.println(ln);
+    System.out.println(sh2);
+    System.out.println(dbl2);
+    System.out.println(i2);
+    System.out.println(sh3);
+    System.out.println(b2);
+    // Task 3
+        var studentsLP =23;
+        var studentsAS = 27;
+        var studentsEA = 30;
+        var allSheetsPaper = 480;
+        var oneStudentSheets = allSheetsPaper/(studentsLP+studentsAS+studentsEA);
+        System.out.println("Task 3");
+        System.out.println("На каждого ученика расчитано " + oneStudentSheets + " листов бумаги");
+
+
     }
  }
